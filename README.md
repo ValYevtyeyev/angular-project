00b42fa25850acd4cf2c424c71259c5c9d9e163a
ValYevtyeyev <val.yevtyeyev243@gmail.com>
2020-10-08 11:40:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-c52tzw-pbp33u
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-c52tzw-pbp33u)
---
+++ src/app/checkout-page/checkout-page.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-checkout-page',
+  templateUrl: './checkout-page.component.html',
+  styleUrls: ['./checkout-page.component.css']
+})
+export class CheckoutPageComponent implements OnInit {
+  items;
+  checkoutForm;
+  totalCost = 0;
+  shippingCost = 0;
+  constructor(private route: ActivatedRoute,
+  private cartService: CartService,
+  private formBuilder: FormBuilder,) {
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+   }
+
+  ngOnInit() {
+    this.items = this.cartService.getItems();
+    
+    for(let i = 0; i < this.items.length; i++){
+      this.totalCost = this.totalCost + this.items[i].price;
+    }
+    this.totalCost = this.totalCost + this.shippingCost;
+  }
+
+  onSubmit(customerData) {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+
+    console.warn('Your order has been submitted', customerData);
+  }
+
+}
